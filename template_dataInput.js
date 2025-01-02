36f97edd66824ae980d35fce5424c43e1bdb8346
TudjiJ <jan.tudjina@gmail.com>
2025-01-02 23:58:00 +0100

    Create template_dataInput.js

---
+++ template_dataInput.js
@@ -0,0 +1,27 @@
+//------------------------------------------------------------------------------------------//
+        
+        const naslov = "Template";
+        const stran = "Družinska zbirka receptov";
+        const photo = "pic05.jpg";
+        const prep = "?";
+        const kolicina = "ŠTEVILO OSEB:"
+        const ingredients = {
+            mleka: 128.666,
+            moke: 100,
+            sladkorja: 20,
+            "pecilnega praška": 4.333,
+            soli: 1.333,
+            masla: 7,
+            jajce: 0.333,
+        };
+        const recipeSteps = [
+    'V manjši posodi penasto vmešamo <strong><span id="jajce2Quantity"></span> <span id="jajce2Name"></span></strong> in <strong><span id="mleka2"></span> mleka</strong>.',
+    'V drugi, večji posodi dobro premešamo <strong><span id="moke2"></span> moke</strong>, <strong><span id="sladkorja2"></span> sladkorja</strong>, <strong><span id="pecilnega_praška2"></span> pecilnega praška </strong> in <strong><span id="soli2"></span> soli</strong>.',
+    'Na ponvi, kjer bomo pekli palačinke, na nizki temperaturi stopimo <strong><span id="masla2"></span> masla</strong>.',
+    'Ob stalnem mešanju, dodamo mokre sestavine k suhim. Z mešanjem nadaljujemo in počasi dodajamo <strong>stopljeno maslo</strong>. Mešamo, da se vse sestavine dobro povežejo.',
+    'Pripravljeno maso pustimo počivati <strong>5min</strong>.<br>Medtem naoljimo in segrejemo ponev nad srednjo temperaturo.',
+    'Z večjo žlico ali dozirnikom na ponev dodajamo maso in pečemo 2-3min na vsaki strani oz. <strong>dokler ne dosežemo zlato rjave barve</strong>.',
+    'Postrežemo s prelivom/sirupom/namazom po izbiri.<br>Odlične so tudi same.'
+];
+
+//------------------------------------------------------------------------------------------//
