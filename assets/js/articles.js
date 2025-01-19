2edbf05449605f356667612336ee67dc119a1b88
TudjiJ <jan.tudjina@gmail.com>
2025-01-19 15:40:53 +0100

    Add files via upload

---
+++ assets/js/articles.js
@@ -0,0 +1,56 @@
+const articles = [
+    {
+        title: "Muffini - ČOKOLADNI",
+        link: "muffini_coko.html",
+        tag: "sladko",
+        fav: "1",
+        image: "muffini_coko.jpeg",
+        alt: "Čokoladni muffini",
+        description: "Prepusti se bogatemu in čokoladnemu okusu domačih čokoladnih mafinov. So hitri in enostavni za pripravo oz. popolni za potešitev želje po sladkem!"
+    },
+    {
+        title: "Palačinke - AMERIŠKE",
+        link: "palacinke_ameriske.html",
+        tag: "sladko",
+        fav: "1",
+        image: "palacinke_ameriske.jpeg",
+        alt: "Ameriške palačinke",
+        description: "Si si zaželel/a neverjetno puhast, zlato zapečen in sladek obrok? Ameriške palačinke so hitre in enostavne za pripravo - kot nalašč za visok kup, premazan/prelit z najljubšimi dodatki!"
+    },
+    {
+        title: "Piškoti - NAVADNI & KRHKI",
+        link: "brst_oh_gorcicna.html",
+        tag: "piskoti",
+        fav: "0",
+        image: "pic01.jpeg",
+        alt: "Navadni krhki piškoti",
+        description: "Praženi brstični ohrovt v gorčični omaki je popolna mešanica hrustljavosti in bogatega okusa. Kombinacija sladkaste karamelizacije in pikantne gorčice ustvarja jed, ki zna navdušiti tudi tiste, ki sicer niso največji ljubitelji brstičnega ohrovta."
+    },
+    {
+        title: "Piškoti - SKALCE",
+        link: "brst_oh_gorcicna.html",
+        tag: "piskoti",
+        fav: "1",
+        image: "pic03.jpeg",
+        alt: "Skalce",
+        description: "Praženi brstični ohrovt v gorčični omaki je popolna mešanica hrustljavosti in bogatega okusa. Kombinacija sladkaste karamelizacije in pikantne gorčice ustvarja jed, ki zna navdušiti tudi tiste, ki sicer niso največji ljubitelji brstičnega ohrovta."
+    },
+    {
+        title: "Testenine z BROKOLIJEM in PIŠČANCEM",
+        link: "test_brokoli_pisc.html",
+        tag: "slano",
+        fav: "0",
+        image: "test_brokoli_pisc.jpeg",
+        alt: "Testenine z brokolijem in piščancem",
+        description: "Kremaste testenine s piščancem in brokolijem združuje nežno teksturo testenin z bogato omako, polno okusov. Sočni koščki piščanca in svež brokoli ustvarjajo popolno harmonijo za okusno in nasitno kosilo."
+    },
+    {
+        title: "BRSTIČNI OHROVT v gorčični omaki",
+        link: "brst_oh_gorcicna.html",
+        tag: "slano",
+        fav: "0",
+        image: "brst_oh_gorcicna.jpeg",
+        alt: "Brstični ohrovt v gorčični omaki",
+        description: "Praženi brstični ohrovt v gorčični omaki je popolna mešanica hrustljavosti in bogatega okusa. Kombinacija sladkaste karamelizacije in pikantne gorčice ustvarja jed, ki zna navdušiti tudi tiste, ki sicer niso največji ljubitelji brstičnega ohrovta."
+    },
+];
